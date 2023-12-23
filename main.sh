5de886429ef66f5790da29188238d5a28a8e680a
Vinícius Yuji <111237473+viniyuji@users.noreply.github.com>
2023-12-23 18:24:11 -0300

    fix: add new flags to compilation

--- main.sh
+++ main.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-eval NEW_PYTHON_VERSION=3.11.7
-eval SHORT_NEW_PYTHON_VERSION=${NEW_PYTHON_VERSION:0:4}
+if [ "$EUID" -ne 0 ]; then
+  echo "Please, run this script with root privileges"
+  exit
+fi
+
+echo "Enter the new Python version: "
+read NEW_PYTHON_VERSION
+SHORT_NEW_PYTHON_VERSION=${NEW_PYTHON_VERSION:0:4}
+
+echo "Installing system dependencies"
+apt update
+apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+echo "Dependencies installed"
 
 echo "Downloading Python"
 mkdir -p /python && cd /python
@@ -11,7 +22,14 @@ echo "Python Downloaded"
 echo "Installing Python"
 tar -xf Python-$NEW_PYTHON_VERSION.tar.xz
 cd Python-$NEW_PYTHON_VERSION
-./configure --enable-optimizations --with-lto
+./configure --enable-optimizations  \
+            --with-lto \
+            --with-computed-gotos \
+            --with-system-ffi \
+            --enable-shared \
+            --prefix=$PWD/run  \
+            --libdir=$PWD/run/lib
+make -j$(nproc)
 make altinstall
 rm -r /python
 echo "Python Installed"
