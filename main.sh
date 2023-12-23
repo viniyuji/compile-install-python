914b58a9bc10bd6e9c41926a1011f135791f38fd
Vinícius Yuji <111237473+viniyuji@users.noreply.github.com>
2023-12-23 11:51:52 -0300

    feat: set Python version at variable NEW_PYTHON_VERSION

--- main.sh
+++ main.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-sudo su
+eval NEW_PYTHON_VERSION=3.11.7
+eval SHORT_NEW_PYTHON_VERSION=${NEW_PYTHON_VERSION:0:4}
 
-mkdir /python
-cd /python
-wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tar.xz
-tar -xf Python-3.11.4.tar.xz
-cd Python-3.11.4
+echo "Downloading Python"
+mkdir -p /python && cd /python
+wget https://www.python.org/ftp/python/$NEW_PYTHON_VERSION/Python-$NEW_PYTHON_VERSION.tar.xz
+echo "Python Downloaded"
+
+echo "Installing Python"
+tar -xf Python-$NEW_PYTHON_VERSION.tar.xz
+cd Python-$NEW_PYTHON_VERSION
 ./configure --enable-optimizations --with-lto
 make altinstall
 rm -r /python
+echo "Python Installed"
 
-cp /usr/local/bin/python3.11 /usr/bin/python3.11
-update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+echo "Setting enviromental variables"
+cp /usr/local/bin/python$SHORT_NEW_PYTHON_VERSION /usr/bin/python$SHORT_NEW_PYTHON_VERSION
+update-alternatives --install /usr/bin/python python /usr/bin/python$SHORT_NEW_PYTHON_VERSION 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$SHORT_NEW_PYTHON_VERSION 1
+echo "Finished"
